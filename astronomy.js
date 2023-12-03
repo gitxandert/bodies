cd7a481bc684f4f597ca36b3fbc6804722c743e3
gitxandert <alexandertoth96@yahoo.com>
2023-12-03 16:17:31 -0500

    Modify astronomy script to accurately log days and months

--- astronomy.js
+++ astronomy.js
@@ -4,12 +4,18 @@ let planets={
   sun:{},moon:{},mercury:{},venus:{},earth:{},mars:{},jupiter:{},saturn:{},uranus:{},neptune:{},pluto:{},
 };
 
-const authString = btoa(`dd97c892-b910-4757-be6e-ec3f92b19505:2cb502c8fb6d3ed668fc3636bb6927f99ec4d08d90260bfc1528cd512104cdb23634b3eec5651c7deff6f2bf9a3a2b8b5a23e731251a14002bbae52f75e9018263ce851acf100e8dbbfffcf9c310280c8e14a240c41586d8444834988ab0de70fdb703c6511eb36e89d6666170458b52`);
+const authString = btoa(`503cdb54-f704-4fae-95d6-172e39e08c82:2cb502c8fb6d3ed668fc3636bb6927f99ec4d08d90260bfc1528cd512104cdb23634b3eec5651c7deff6f2bf9a3a2b8b5a23e731251a14002bbae52f75e90182a5c42e4e14f34b8376433d43077959cb2de68b3ffe767e8ece0301ea42ee0414a268489223afde73dde172cc3ea9c270`);
 async function getPlanets(){
 
   const date = new Date();
   let month= date.getMonth()+1;
+  if(month<10){
+    month=`0${month}`;
+  }
   let day= date.getDate();
+  if(day<10){
+    day=`0${day}`;
+  }
   let year=date.getFullYear();
   let todays_date=`${year}-${month}-${day}`;
   console.log(todays_date);
@@ -25,7 +31,7 @@ async function getPlanets(){
   const res = await fetch("https://api.astronomyapi.com/api/v2/bodies/positions?" + new URLSearchParams({
     "latitude": '39.163587',
     "longitude": '-86.499374',
-    "elevation": '243',
+    "elevation": '0',
     "from_date": todays_date,
     "to_date": todays_date,
     "time": the_hour
