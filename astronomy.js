51373b8d2c4a3d28b11a65c9bfb813865154a0fa
gitxandert <alexandertoth96@yahoo.com>
2023-12-05 19:19:20 -0500

    Add latitude and longitude variables

--- astronomy.js
+++ astronomy.js
@@ -5,6 +5,10 @@ let planets={
 };
 
 const authString = btoa(`503cdb54-f704-4fae-95d6-172e39e08c82:2cb502c8fb6d3ed668fc3636bb6927f99ec4d08d90260bfc1528cd512104cdb23634b3eec5651c7deff6f2bf9a3a2b8b5a23e731251a14002bbae52f75e90182a5c42e4e14f34b8376433d43077959cb2de68b3ffe767e8ece0301ea42ee0414a268489223afde73dde172cc3ea9c270`);
+
+let latitude;
+let longitude;
+
 async function getPlanets(){
 
   const date = new Date();
@@ -32,8 +36,8 @@ async function getPlanets(){
   console.log(the_hour);
 
   const res = await fetch("https://api.astronomyapi.com/api/v2/bodies/positions?" + new URLSearchParams({
-    "latitude": '39.163587',
-    "longitude": '-86.499374',
+    "latitude": latitude,
+    "longitude": longitude,
     "elevation": '0',
     "from_date": todays_date,
     "to_date": todays_date,
@@ -48,11 +52,6 @@ async function getPlanets(){
     });
 
   obj = await res.json();
-
-  //altitude = -90 (invisible) to 90 (visible)
-  //azimuth = 0-180 (rising) 180-360 (setting)
-  //88 constellations!!! make list, figure out what to do with them :P
-  //planets can be melodies or something; 88 constellations can be particular tunings (if RNBO doesn't flip out...)
   
   let i=0;
 
